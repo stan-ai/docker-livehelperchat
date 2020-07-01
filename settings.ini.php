eee1d0330c72411b7c032e4ca1d6c3bb4b53f3e1
Keyvan M. Sadeghi <keyvan.m.sadeghi@gmail.com>
2020-07-01 12:07:25 -0400

    Create settings.ini.php

---
+++ settings.ini.php
@@ -0,0 +1,8 @@
+<?php 
+
+return array(
+    'ahenviroment' => false, // Is it automated hosting enviroment, not support in twilio at the moment
+    'debug' => true, // If debug enabled you can find details in default.log
+);
+
+?>
